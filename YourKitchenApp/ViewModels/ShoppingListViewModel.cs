dfdd70a9c5a23c8a6044615eb42f2c8bd603389e
Chloemobiledev <chloemobiledev@gmail.com>
2023-04-13 01:05:37 +0100

    Added alert for expiry date checking

---
+++ YourKitchenApp/ViewModels/ShoppingListViewModel.cs
@@ -0,0 +1,15 @@
+namespace YourKitchenApp.ViewModels;
+
+public class ShoppingListViewModel : ContentPage
+{
+	public ShoppingListViewModel()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
